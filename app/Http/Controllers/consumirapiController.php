d0b0404aff5821a4f94f0fb4888aec39086acc77
agildojunior <agildo500@hotmail.com>
2022-08-13 14:02:30 -0300

    Mudanças para consumir a API atualizada

--- app/Http/Controllers/consumirapiController.php
+++ app/Http/Controllers/consumirapiController.php
@@ -12,10 +12,6 @@ class consumirapiController extends Controller
     public function adicionarcorridas(Request $request){
         $dados = $request->all();
         $response = Http::post('http://127.0.0.1:8090/corridas',[
-        'id_empresa' => 3,
-        'id_taxis' => null,
-        'status_corrida' => "Solicitada",
-        'cliente' => $request->input('cliente'),
         'origem' => $request->input('origem'),
         'destino' => $request->input('destino'),
         ]);
--- resources/views/taxi.blade.php
+++ resources/views/taxi.blade.php
@@ -36,10 +36,6 @@
             <form class="formulario" action="/adicionarcorridas"  method="get"> 
 
                 <div class="form-group">
-                    <label>Nome</label>
-                    <input type="text" placeholder="Digite seu nome." name="cliente" required>
-                </div>
-                <div class="form-group">
                     <label>Origem</label>
                     <input type="text" placeholder="Onde você está agora?" name="origem" required>
                 </div>
