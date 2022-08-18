892f64b893ce69687b295816f7a29f0c1cbdc541
evertonleite <everttonleitte@gmail.com>
2022-08-18 16:24:23 -0300

    Alterando condição para Login com Conta Google

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -17,20 +17,23 @@ class GoogleAuthController extends Controller
 
             $google_user = Socialite::driver('google')->user();
 
-            $user = User::where('google_id', $google_user->getId())->first();
+            $user = User::where('email', $google_user->getEmail())->first();
 
             if(!$user) {
+                
                 $new_user = User::create([
                     'name' => $google_user->getName(),
                     'email' => $google_user->getEmail(),
                     'google_id' => $google_user->getId()
                 ]);
+                
 
                 Auth::login($new_user);
 
                 return redirect()->intended('/inicio');
             }
             else{
+    
                 Auth::login($user);
 
                 return redirect()->intended('/inicio');
